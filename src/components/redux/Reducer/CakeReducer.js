cf8428b4311ea2352613c6a1376388c0de98d480
unknown <sk.manoj322@gmail.com>
2022-12-27 15:57:50 +0530

    completed the reducer

---
+++ src/components/redux/Reducer/CakeReducer.js
@@ -0,0 +1,16 @@
+import { BUY_CAKE } from "../Cakes/cakeTypes";
+const initialState = {
+  numOfCake: 10,
+};
+const CakeReducer = (state = initialState, action) => {
+  switch (action.types) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numOfCake: state.numOfCake - 1,
+      };
+    default:
+      return state;
+  }
+};
+export default CakeReducer;
