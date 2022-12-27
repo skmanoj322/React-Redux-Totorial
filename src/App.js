b8f8b394c895af1c7058e5c080f49f1a0deeb136
unknown <sk.manoj322@gmail.com>
2022-12-27 15:43:59 +0530

    Action

--- src/App.js
+++ src/App.js
@@ -2,37 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 import redux from "redux";
 import { legacy_createStore as createStore } from "redux";
+import CakeComponents from "./components/CakeComponent";
 
 function App() {
-  const createStore = redux.legacy_createStore;
-  const BUY_CAKE = "BUY_CAKE";
-  function buycake() {
-    return {
-      type: BUY_CAKE,
-      info: "Frist redux action",
-    };
-  }
-  const initialState = {
-    numOfCakes: 10,
-  };
-  const reducer = (state = initialState, action) => {
-    switch (action.type) {
-      case BUY_CAKE:
-        return {
-          ...state,
-          numOfCakes: state.num,
-        };
-      default:
-        return state;
-    }
-  };
-  const store = createStore(reducer);
-  const showState = () => {
-    console.log("Initial State", store.getState());
-  };
   return (
     <div className="App">
-      <button onClick={showState}>click</button>
+      <CakeComponents />
     </div>
   );
 }
---
+++ src/components/CakeComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CakeComponents = () => {
+  return (
+    <div>
+      <h2>Number of cakes</h2>
+      <button>Buy cake</button>
+    </div>
+  );
+};
+
+export default CakeComponents;
